a94f75d8c6b94cb95fd67b4ac8430f064074acd7
Alyssa Mobley <alyssamoble2004@gmail.com>
2026-07-06 15:05:14 -0400

    modified signup

--- Frontend/src/pages/SignUp/Signup.jsx
+++ Frontend/src/pages/SignUp/Signup.jsx
@@ -38,7 +38,7 @@ function Signup() {
     };
 
     try {
-        const response = await fetch(`${import.meta.env.VITE_APP_URL}/api/auth/signup`, {
+        const response = await fetch(`${import.meta.env.VITE_API_URL}/api/auth/signup`, {
             method: "POST",
             headers: { "Content-Type": "application/json" },
             body: JSON.stringify(signupData),
